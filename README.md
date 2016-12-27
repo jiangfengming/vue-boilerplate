ff9bb9d402548a5eb2955f1f3e40e4821edbe334
fenivana <fenivana@noindoin.com>
2016-12-27 14:38:30 +0800

    Customizable output.publicPath; Add `npm run local` shortcut;

--- README.md
+++ README.md
@@ -1,19 +1,40 @@
 # An Ordinary Vue Boilerplate
 
+
 ## Install Dependencies
+
 ```sh
 npm install
 ```
 
+
 ## Development
+
 ```sh
 npm run dev
 ```
 
+Will load `config/dev.js` as config file.
+
+```sh
+npm run local
+```
+
+Will load `config/local.js`, it's an alias of
+
+```sh
+npm run dev --config=local
+```
+
+
 ## Build
+
 ```sh
 npm run build
 ```
+
+Will load `config/default.js`.
+
 
 ## License
 [MIT](http://opensource.org/licenses/MIT)
--- config/default.js
+++ config/default.js
@@ -8,20 +8,5 @@ module.exports = {
     thirdPartyApiKey: 'abcdefg123456'
   },
 
-  devServer: {
-    port: 8100,
-    proxy: {
-      '/api/auth/': {
-        target: 'http://api.example.dev',
-        changeOrigin: true,
-        pathRewrite: { '^/api': '' }
-      },
-
-      '/api/pay/': {
-        target: 'http://pay.example.dev',
-        changeOrigin: true,
-        pathRewrite: { '^/api': '' }
-      }
-    }
-  }
+  publicPath: 'http://cdn.example.com/assets/'
 }
